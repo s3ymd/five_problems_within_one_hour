4817157d41853b05037a4ccfadeab629eb58662b
user <user>
2017-08-01 11:50:41 +0900

    another algorithm

--- lib/question5.rb
+++ lib/question5.rb
@@ -4,17 +4,26 @@
 # 結果が100となるあらゆる組合せを出力するプログラムを記述せよ。
 # 例えば、1 + 2 + 34 – 5 + 67 – 8 + 9 = 100となる。
 
-def exp100
-  nums = *1..9
-  ops = ['+', '-', '']
+# def exp100
+#   nums = *1..9
+#   ops = ['+', '-', '']
+#   nums.inject do |memo, n|
+#     ops.map do |op|
+#       Array(memo).map do |pat|
+#         "#{pat}#{op}#{n}"
+#       end
+#     end.flatten
+#   end.select { |exp| eval(exp) == 100 }
+# end
 
-  nums.inject do |memo, n|
-    ops.map do |op|
-      Array(memo).map do |pat|
-        "#{pat}#{op}#{n}"
-      end
-    end.flatten
-  end.select { |exp| eval(exp) == 100 }
+def exp100
+  ops = ['+', '-', '']
+  [*2..9]
+    .inject([[1]]) { |a, b| a << ops << [b] }
+    .inject(&:product)
+    .map(&:flatten)
+    .map(&:join)
+    .select { |e| eval(e) == 100 }
 end
 
 # puts exp100
